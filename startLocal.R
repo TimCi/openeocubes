18027cc6aadcc972d8146b5611e35bd300454089
michael_b <m.brueggemann7@web.de>
2023-12-23 23:49:37 +0100

    added test workspace

--- startLocal.R
+++ startLocal.R
@@ -5,6 +5,7 @@ remotes::install_local("./", dependencies = TRUE, force = TRUE)
 library(openeocubes)
 
 config <- SessionConfig(api.port = 8000, host = "127.0.0.1")
-
+# set workspace for testing
+config$workspace.path = paste0(getwd(), "/test_workspace")
 createSessionInstance(config)
 Session$startSession()
