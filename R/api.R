da8b4f7f9003e0609fdf974a992b60749fa20523
michael_b <m.brueggemann7@web.de>
2024-01-14 14:03:19 +0100

    add process to directly load cubes from .nc files

--- R/api.R
+++ R/api.R
@@ -379,4 +379,5 @@ addEndpoint = function() {
   Session$assignProcess(predict_model)
   Session$assignProcess(fill_missing_values)
   Session$assignProcess(apply_prediction)
+  Session$assignProcess(load_netCDF_cube)
 }
--- R/processes.R
+++ R/processes.R
@@ -1895,3 +1895,47 @@ apply_prediction <- Process$new(
     return(cube)
   }
 )
+
+
+
+#' load_netCDF_cube
+load_netCDF_cube <- Process$new(
+  id = "load_netCDF_cube",
+  description = "Loads netCDF cube from user workspace via its file name.",
+  categories = as.array("cubes"),
+  summary = "Load netCDF cube..",
+  parameters = list(
+    Parameter$new(
+      name = "cube_id",
+      description = "Filename of the netCDF cube.",
+      schema = list(
+        type = "string"
+      )
+    )),
+  returns = eo_datacube,
+  operation = function(cube_id, job) {
+    message("\nload_netCDF_cube called...")
+    message("\ncube_id:")
+    message(cube_id)
+
+    tryCatch({
+      message("\nTry loading netCDF cube...")
+
+      cubepath = paste0(Session$getConfig()$workspace.path, "/", cube_id, ".nc")
+
+      message("\npath to cube: ", cubepath)
+
+      cube = gdalcubes::ncdf_cube(cubepath)
+
+      message("netCDF cube loaded!")
+    },
+    error = function(err)
+    {
+      message("An Error occured!")
+      message(toString(err))
+      stop()
+    })
+
+    return(cube)
+  }
+)
